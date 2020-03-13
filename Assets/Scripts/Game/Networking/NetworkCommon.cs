c160d14492cc874b109ad541126d3e5263ca0ccf
Zionch <chengchonz@yahoo.com.tw>
2020-03-13 12:16:44 +0800

    Transport interface & event type

---
+++ Assets/Scripts/Game/Networking/NetworkCommon.cs
@@ -0,0 +1,25 @@
+﻿public struct TransportEvent
+{
+    public enum Type
+    {
+        Data,
+        Connect,
+        Disconnect
+    }
+    public Type type;
+    public int ConnectionId;
+    public byte[] Data;
+    public int DataSize;
+}
+
+public interface INetworkTransport
+{
+    void Connect();
+    void Disconnect();
+    bool NextEvent(ref TransportEvent e);
+    void SendData(int connectionId, byte[] data, int sendSize);
+    string GetConnectionDescription(int connectionId);
+    void Update();
+    void Shutdown();
+}
+
