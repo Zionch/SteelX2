bfa6e4e100a11e06a83e97a0dcd9494d5802a139
Zionch <chengchonz@yahoo.com.tw>
2020-03-14 13:17:23 +0800

    modify transport interface

--- Assets/Scripts/Game/Networking/NetworkCommon.cs
+++ Assets/Scripts/Game/Networking/NetworkCommon.cs
@@ -2,24 +2,29 @@
 {
     public enum Type
     {
-        Data,
+        Data = 10,
         Connect,
         Disconnect
     }
     public Type type;
     public int ConnectionId;
     public byte[] Data;
-    public int DataSize;
+
+    public TransportEvent(Type type, int connectionId, byte[] data) {
+        this.type = type;
+        this.ConnectionId = connectionId;
+        this.Data = data;
+    }
 }
 
 public interface INetworkTransport
 {
     void Connect();
     void Disconnect();
+    void Shutdown();
     bool NextEvent(ref TransportEvent e);
-    void SendData(int connectionId, byte[] data, int sendSize);
+    void SendData(int connectionId, TransportEvent.Type type, byte[] data);
     string GetConnectionDescription(int connectionId);
     void Update();
-    void Shutdown();
 }
 
